3085708e1109539fe77b72613807e3813659dcf4
qing.zhao <qing.zhao@100credit.com>
2021-06-25 17:06:59 +0800

    feat: update

--- src/main/java/com/sb/plugin/handler/DefaultFieldTypeHandler.java
+++ src/main/java/com/sb/plugin/handler/DefaultFieldTypeHandler.java
@@ -1,19 +1,24 @@
 package com.sb.plugin.handler;
 
+import com.sb.plugin.util.ResolveUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Created by zhaoqing on 2021-06-21 15:29
  */
 public class DefaultFieldTypeHandler extends BaseFieldTypeHandler<Object> {
 
+    private static final Log log = LogFactory.getLog(DefaultFieldTypeHandler.class);
     @Override
     public Object setParam(String fieldName, Object par) {
-        //log.info("par {} type is {}", fieldName, ResolveUtil.getType(par));
+        log.info(String.format(" [%s] type is [%s]", fieldName, ResolveUtil.getType(par)));
         return par;
     }
 
     @Override
     public Object getResult(String fieldName, Object val) {
-        //log.info("val {} type is {}", fieldName, ResolveUtil.getType(val));
+        log.info(String.format(" [%s] type is [%s]", fieldName, ResolveUtil.getType(val)));
         return val;
     }
 
